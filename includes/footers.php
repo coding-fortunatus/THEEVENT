4a259a450560a84250520766b994a749cda5a621
Obasa TimothyE <horbassa123@gmail.comm>
2023-08-09 10:32:07 -0400

    readme.txt

---
+++ includes/footers.php
@@ -0,0 +1,65 @@
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h1><a href="index.php">SP<span>AS</span></a></h1>
+            <!-- <img src="assets/img/logo.png" alt="TheEvenet"> -->
+            <p>It is with great delight that I welcome you to the School of Pure and Applied Science comprising Departments of Science Laboratory and Technology, Computer Sciences, Mathematics & Statistics, Nutrition and Dietetics, Food Technology, Library & Information Sciences, Leisure and Tourism  and Agriculture Technology. The Current Dean of the School is Dr. Oyede, R.T. </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="https://federalpolyilaro.edu.ng">Federal Polytechnic Ilaro</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="https://federalpolyilaro.edu.ng/school/school-of-pure-and-applied-sciences">FPI School of Pure & Applied Sciences</a></li>
+              
+            </ul>
+          </div>
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br>
+              <strong>Phone:</strong> +234 <br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright School of Pure and Applied Sciences. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
